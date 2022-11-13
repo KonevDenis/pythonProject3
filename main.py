3a65b366907a1a6385c8b8afafbf84ba845e627e
KonevDenis <konevd433@gmail.com>
2022-11-13 19:09:37 +0300

    вв

--- main.py
+++ main.py
@@ -3,38 +3,41 @@ f = open('task2_data.dat', 'r')  # открываем файл
 stars = f.read().split('\n')  # считываем весь файл, разделяя по переходам строк (используем именно read, потому что readline и readlines создают списки)
 
 #2)Вывести на экран имена объетов, присутствующих в каталоге (без дублей!!!) и фильтров,в которых есть данные для тех или иных объектов.
-entries = []  # временный массив
-for entry in stars:  # перебираем все ряды
-    entries.append(entry.split())  # разделяем ряды по пробелам
-entries = entries[1:]  # удаляем первое значение (заголовки)
-stars = entries  # заменяем изначальный массив временным
-entries = []  # обнуляем временный массив
-print(stars)
+item = []  # временный массив # item - записи, котрорые будем использовать input_data - входные данные
+for input_data in stars:  # перебираем все ряды
+    item.append(input_data.split())  # разделяем ряды по пробелам
+item = item[1:]  # удаляем первое значение (заголовки)
+#ПРО СРЕЗЫ item[START:STOP:STEP]             Это способ запомнить, что начальное значение является инклюзивным, а конечное - исключающим.
+stars = item  # заменяем изначальный массив временным
+item = []  # обнуляем временный массив
 
 # for i in stars:
 #     names = i[:1]
 #     names_obj = names.sort()
 #     print(names)
-for entry in stars:
+print(item)
+for input_data in stars:
     name = ''  # формируем одинаковое имя
-    if len(entry) > 4:  # в имени есть пробел
-        name = entry[0] + ' ' + entry[1]  # первая часть имени плюс пробел плюс вторая часть имени
-        entry = [name] + entry[2:]  # обновляем ряд в соответствии с новым именем
-    elif len(entry) < 4:  # ряд не рассматриваем
+    if len(input_data) > 4:  # в имени есть пробел
+        name = input_data[0] + ' ' + input_data[1]  # первая часть имени плюс пробел плюс вторая часть имени
+        input_data = [name] + input_data[2:]  # обновляем ряд в соответствии с новым именем
+    elif len(input_data) < 4:  # ряд не рассматриваем
         continue
     else:
-        name = entry[0]
+        name = input_data[0]
+        print(name)
 
     # SU_Hor, su hor, SU Hor => suhor - преобразовываем все варианты записи имен в один
-    name = name.replace('_', '')
-    name = name.replace(' ', '')
-    name = name.lower()
+    name = name.replace('_', '') # replace  помогает нам заменить ненужные символы
+    name = name.replace(' ', '') # в агрументах через запятую сначала ставим old через запятую new
+    name = name.lower() #преобразует все символы верхнего регистра в строке в символы нижнего регистра
     name = name[:2].upper() + ' ' + name[2:].title()  # снова преобразовываем, suhor => SU Hor
-    entry[0] = name  # обновляем ряд
+    input_data[0] = name  # обновляем ряд
 
-    entry[1] = float('24' + entry[1])  # преобразуем дату и звездную величину в числа из строк
-    entry[3] = float(entry[3])
+    input_data[1] = float('24' + input_data[1])  # преобразуем дату и звездную величину в числа из строк
+    input_data[3] = float(input_data[3])
 
-    entries.append(entry)  # обновляем временный массив
-    name = entry[0]
-    print(name)
+    item.append(input_data)  # обновляем временный массив
+    name = input_data[0]
+    stars = item  # замена
+    item = []  # обнуление
